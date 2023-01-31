dd0f550b5a4d3f84496ff5f8d7e2f89a16b9ec16
Martinez-R <martinez.99.ramiro@gmail.com>
2023-01-31 10:26:20 -0300

    Ejercicios Resueltos

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,94 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	let numuno;
+
+	let numdos;
+
+	let enterouno;
+
+	let suma;
+	
+		numuno = txtIdNumeroUno.value;
+
+		numdos = txtIdNumeroDos.value;
+
+		enterouno = parseInt (numuno);
+
+		enterodos = parseInt (numdos);
+
+		suma = enterouno + enterodos;
+
+		alert ("La suma es: " + suma);
 }
 
 function restar()
 {
+	let numuno;
+
+	let numdos;
+
+	let enterouno;
+
+	let resta;
 	
+		numuno = txtIdNumeroUno.value;
+
+		numdos = txtIdNumeroDos.value;
+
+		enterouno = parseInt (numuno);
+
+		enterodos = parseInt (numdos);
+
+		resta = enterouno - enterodos;
+
+		alert ("La resta es: " + resta);
 }
 
 function multiplicar()
 { 
+	let numuno;
+
+	let numdos;
+
+	let enterouno;
+
+	let producto;
 	
+		numuno = txtIdNumeroUno.value;
+
+		numdos = txtIdNumeroDos.value;
+
+		enterouno = parseInt (numuno);
+
+		enterodos = parseInt (numdos);
+
+		producto = enterouno * enterodos;
+
+		alert ("La multiplicación es: " + producto);
 }
+
 
 function dividir()
 {
+	let numuno;
+
+	let numdos;
+
+	let enterouno;
+
+	let cociente;
 	
+		numuno = txtIdNumeroUno.value;
+
+		numdos = txtIdNumeroDos.value;
+
+		enterouno = parseInt (numuno);
+
+		enterodos = parseInt (numdos);
+
+		cociente = enterouno / enterodos;
+
+		alert ("La división es: " + cociente);
 }
 
